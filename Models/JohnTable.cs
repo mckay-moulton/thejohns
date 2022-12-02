6ba05fe5aee64ffa8945066594e60a42d63cba80
mckay-moulton <79293626+mckay-moulton@users.noreply.github.com>
2022-12-02 09:55:20 -0700

    final john

--- Models/JohnTable.cs
+++ Models/JohnTable.cs
@@ -25,5 +25,9 @@ namespace thejohns.Models
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public string Image { get; set; }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public string? About
+        {
+            get; set;
+        }
     }
 }
